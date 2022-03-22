2be9177196f6af73ffee7a23f536fe4c92064a7e
Niloy Kumar Kundu <nkundu191071@bscse.uiu.ac.bd>
2022-03-22 14:28:37 +0600

    Lab -3

---
+++ Class 3/4/7-seg.c
@@ -0,0 +1,15 @@
+#include <mega32.h>
+#include <delay.h>
+void main(void)
+{
+DDRA = 0xFF;
+DDRB = 0xFF;
+while (1) {
+PORTA= 0b00000001;
+PORTB= 0b11111111;
+delay_ms(100);
+PORTA= 0b00000001;
+PORTB= 0b11111111;
+
+}
+}
